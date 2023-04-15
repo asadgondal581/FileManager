9fe0d9f57d86df495ee9e2ca0a37cde6ab937463
unknown <asadgondal581@gmail.com>
2023-04-15 16:27:12 +0500

    Initial commit..

---
+++ app/src/main/java/com/example/filemanager/AboutActivity.kt
@@ -0,0 +1,19 @@
+package com.example.filemanager
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.filemanager.databinding.ActivityAboutBinding
+
+
+class AboutActivity : AppCompatActivity() {
+    @SuppressLint("SetTextI18n")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setTheme(MainActivity.themesList[MainActivity.themeIndex])
+        val binding = ActivityAboutBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        supportActionBar?.title = "About"
+        binding.aboutText.text = "Developed By: Harsh H. Rajpurohit \n\nIf you want to provide feedback, I will love to hear that."
+    }
+}
